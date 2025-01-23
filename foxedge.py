75a78142b4c077c9f333e2634747780176c7e603
weballin02 <34046845+weballin02@users.noreply.github.com>
2025-01-23 14:28:45 -0800

    Update foxedge.py

--- foxedge.py
+++ foxedge.py
@@ -876,7 +876,7 @@ def main():
 
         # Run League Pipeline for Selected League (if chosen)
         if not st.session_state.get('selected_page'):
-        run_league_pipeline(st.session_state['selected_page'])
+            run_league_pipeline(st.session_state['selected_page'])
         else:
             display_homepage()
 
